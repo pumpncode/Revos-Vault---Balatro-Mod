6ff6d44f0b31dba10492046f5de401f9b1cab3f1
CodeRevo <devrimulasyurtsever00@gmail.com>
2025-08-07 18:27:39 +0300

    Pitch Black Stake moved to experimental

--- items/experimental.lua
+++ items/experimental.lua
@@ -2,3 +2,29 @@
 
 
 
+SMODS.Stake{
+    key = "pitch_black",
+    atlas = "stakes",
+    pos = {x=2,y=1},
+    colour = HEX("fdffa8"),
+    sticker_atlas = "enh",
+    sticker_pos = {x=6,y=3},
+    applied_stakes = { "crv_overt" },
+    shiny = true,
+    unlocked = false,
+    prefix_config = { applied_stakes = { mod = false} },
+    loc_txt = {
+        name = "Overtime Stake",
+        text = {"{C:attentionAll{} Jokers can have {C:money}Overtime"},
+                sticker = {
+                    name = "Overtime Sticker",
+                    text = {"Used this Joker",
+                            "to win on {C:attention}Overtime",
+                            "{C:attention}Stake{} difficulty"}
+
+                }
+    },
+    modifiers = function()
+        G.GAME.modifiers.enable_no_vouchers = true
+    end
+}
